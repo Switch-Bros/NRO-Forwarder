f8e75a10862c00eb14bfc4c71a0a136d454c595a
mrdude2478@gmail.com <mrdude2478@gmail.com>
2024-10-28 00:22:20 +0000

    Fix Readme

--- NRO Forwarder/Properties/AssemblyInfo.cs
+++ NRO Forwarder/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NRO Forwarder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Nintendo Switch NRO forwarder creator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DudeWarez")]
 [assembly: AssemblyProduct("NRO Forwarder")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  MrDude")]
+[assembly: AssemblyTrademark("DudeWarez")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -28,5 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-GB")]
